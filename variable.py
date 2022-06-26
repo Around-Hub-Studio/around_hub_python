8b4d34cebd90e04b6d2b0b8e5d66e6b99af74f6a
Flature <jungwoo5394@gmail.com>
2022-06-26 20:20:12 +0900

    gitignore 추가 및 변수 다루기 예제 추가

---
+++ variable.py
@@ -0,0 +1,59 @@
+
+
+# 변수 선언 방법
+tv = 1000
+mobile = 500
+_mobile = 400
+tv2 = 2000
+# 2tv = 2000
+tv3, mobile2 = 5000, 2000
+bill = tv + mobile
+
+tv_double_quote_description = "tv 상품입니다."
+tv_single_quote_description = 'tv 상품입니다.'
+
+print(bill)
+
+# 숫자 타입
+
+# boolean 타입
+is_true = True
+is_false = False
+
+# 정수(int) 타입
+a = 42
+b = -42
+c = 0x2a  # 16진수 (0x로 시작)
+d = 0o52  # 8진수 (0o로 시작)
+e = 0b101010  # 2진수 (0b로 시작)
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+# 실수(float) 타입
+a = 1.23
+b = -1.23
+
+print(a)
+print(b)
+
+# 사칙 연산
+a = 3
+b = 4
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+
+# 문자열(String) 타입
+# 문자열 안에 작은 따옴표나 큰 따옴표를 포함시키고 싶을 경우에 취사 선택하여 사용
+# 또는 줄바꿈 문자(\n)을 넣는 것이 번거로울 경우 ''' 또는 """을 사용
+a = "Around Hub Studio"
+b = 'Around Hub Studio'
+c = """Around Hub Studio"""
+d = '''Around Hub Studio'''
